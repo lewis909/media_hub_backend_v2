9360cfc9195480f6e56692e6646c3e9983a16c42
lewis909 <lewis.dt@gmail.com>
2016-11-08 14:46:45 +0000

    fixed round robin issue.

--- task_prep.py
+++ task_prep.py
@@ -4,55 +4,68 @@ import config
 import os.path
 import xml.dom.minidom as dom
 
-move_time = time.ctime()
-nc_1 = len(config.node_1_count)
-nc_2 = len(config.node_2_count)
-nc_3 = len(config.node_3_count)
-nc_4 = len(config.node_4_count)
+if config.prep_xml:
 
-load = [nc_1, nc_2, nc_3, nc_4]
+    for file in config.prep_xml:
 
-print(move_time + ': Load Balance Status: Node_1 = ' + str(load[0]) + ', Node_2 = ' + str(load[1]) + ', Node_3 = ' + str(load[2]) + ', Node_4 =' + str(load[3]))
+        move_time = time.ctime()
 
-for file in config.prep_xml:
+        doc = dom.parse(file)
+        filename = doc.getElementsByTagName("source_filename")
+        fname = filename[0].firstChild.nodeValue
+        base = os.path.splitext(os.path.basename(file))[0]
+        src_repo = config.repo + fname + '.mp4'
+        scr_tar = config.prep + base + '.mp4'
 
-    doc = dom.parse(file)
-    filename = doc.getElementsByTagName("source_filename")
-    fname = filename[0].firstChild.nodeValue
-    base = os.path.splitext(os.path.basename(file))[0]
-    src_repo = config.repo + fname + '.mp4'
-    scr_tar = config.prep + base + '.mp4'
+        xml_base = os.path.basename(file)
+        vid_base = os.path.basename(scr_tar)
 
-    if min(load) == nc_1:
+        nc_1 = len(os.listdir(config.node_1_path))
+        nc_2 = len(os.listdir(config.node_2_path))
+        nc_3 = len(os.listdir(config.node_3_path))
+        nc_4 = len(os.listdir(config.node_4_path))
 
-        print("Task assigned to Node 1")
+        load = [nc_1, nc_2, nc_3, nc_4]
 
-        shutil.copy(src_repo, scr_tar)
-        shutil.move(file, config.node_1_path)
-        shutil.move(scr_tar, config.node_1_path)
+        if min(load) == nc_1:
 
-        print(move_time + ": The following files have been moved to Node 1:" '\n' + file + '\n' + scr_tar)
+            print("Task assigned to Node 1")
 
-    elif min(load) == nc_2:
+            shutil.copy(src_repo, scr_tar)
+            shutil.move(file, config.node_1_path)
+            shutil.move(scr_tar, config.node_1_path)
 
-        shutil.copy(src_repo, scr_tar)
-        shutil.move(file, config.node_2_path)
-        shutil.move(scr_tar, config.node_2_path)
+            print(move_time + ": The following files have been moved to Node 1:" '\n' + xml_base + '\n' + vid_base)
 
-        print(move_time + ": The following files have been moved to Node 2:" '\n' + file + '\n' + scr_tar)
+        elif min(load) == nc_2:
 
-    elif min(load) == nc_3:
+            print("Task assigned to Node 2")
 
-        shutil.copy(src_repo, scr_tar)
-        shutil.move(file, config.node_3_path)
-        shutil.move(scr_tar, config.node_3_path)
+            shutil.copy(src_repo, scr_tar)
+            shutil.move(file, config.node_2_path)
+            shutil.move(scr_tar, config.node_2_path)
 
-        print(move_time + ": The following files have been moved to Node 3:" '\n' + file + '\n' + scr_tar)
+            print(move_time + ": The following files have been moved to Node 2:" '\n' + xml_base + '\n' + vid_base)
 
-    elif min(load) == nc_4:
+        elif min(load) == nc_3:
 
-        shutil.copy(src_repo, scr_tar)
-        shutil.move(file, config.node_4_path)
-        shutil.move(scr_tar, config.node_4_path)
+            print("Task assigned to Node 3")
 
-        print(move_time + ": The following files have been moved to Node 4:" '\n' + file + '\n' + scr_tar)
+            shutil.copy(src_repo, scr_tar)
+            shutil.move(file, config.node_3_path)
+            shutil.move(scr_tar, config.node_3_path)
+
+            print(move_time + ": The following files have been moved to Node 3:" '\n' + xml_base + '\n' + vid_base)
+
+        elif min(load) == nc_4:
+
+            print("Task assigned to Node 4")
+
+            shutil.copy(src_repo, scr_tar)
+            shutil.move(file, config.node_4_path)
+            shutil.move(scr_tar, config.node_4_path)
+
+            print(move_time + ": The following files have been moved to Node 4:" '\n' + xml_base + '\n' + vid_base)
+
+else:
+    print('No files')

319b5724df90b6bf7b4e7b08aacd415c40987411
lewis909 <lewis.dt@gmail.com>
2016-11-21 15:30:07 +0000

    work in progress

--- test_xml.py
+++ test_xml.py
@@ -31,9 +31,6 @@ def parse_xml_2(file_input):
 
     ffmpeg_start_str = 'ffmpeg -i test.mp4 '
     ffmpeg_seg_cmd = 'FLAG-ss in_point -t out_point target_file.mp4 '
-
-
-
     ffmpeg_cmd = ffmpeg_start_str + ffmpeg_seg_cmd * segments_no
 
     return segments, ffmpeg_cmd
@@ -43,12 +40,13 @@ test_1, test_2 = parse_xml_2(file)
 f = str(test_1).find('seg_1_in')
 x = str(test_1).find(']', f)
 duration_values = str(test_1)
-print(duration_values)
+#print(duration_values)
 
 
 def find_seg_in_point(dur_string, seg_number):
     seg_start_list = []
     seg_duration_list = []
+    c_file_list = []
     while True:
         for i in range(seg_number):
             seg = 'seg_%d_in =' % (i+1)
@@ -58,20 +56,26 @@ def find_seg_in_point(dur_string, seg_number):
                 seg_1_in = str(dur_string)[seg_1_in_s:seg_1_in_e - 1]
                 seg_start_list.append(str(seg_1_in).replace(seg, '-ss'))
         for i in range(seg_number):
-            seg = 'seg_%d_dur =' % (i+1)
+            seg = 'seg_%d_dur =' % (i + 1)
+            file_num ='C_%d_FILE.MP4' % (i + 1)
+            c_file_list.append(file_num)
             seg_1_dur_s = str(dur_string).find(seg)
             if seg_1_dur_s > 0:
                 seg_1_dur_e = str(dur_string).find(']', seg_1_dur_s)
                 seg_1_dur = str(dur_string)[seg_1_dur_s:seg_1_dur_e - 1]
                 seg_duration_list.append(str(seg_1_dur).replace(seg, '-t'))
         break
-    print(seg_start_list)
-    print(seg_duration_list)
-    return seg_start_list, seg_duration_list
+    #print(seg_start_list)
+    #print(seg_duration_list)
+
+    in_point_and_dur = list(zip(seg_start_list, seg_duration_list, c_file_list))
+    #print(in_point_and_dur)
+
+    return seg_start_list, seg_duration_list, in_point_and_dur
 
 
-find_seg_in_point(duration_values, 4)
-
+t_1, t_2, t_3 = find_seg_in_point(duration_values, 4)
+print('ffmpeg -i FILE_NAME ' + str(t_3)[3:-2].replace("'", '').replace(',', '').replace('(', '').replace(')', ''))
 # print(test_2)
 # print(f)
 # print(x)

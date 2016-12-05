64d891fb858c51e3c4e9c920d82e0b0d335c14ce
lewis909 <lewis.dt@gmail.com>
2016-12-05 14:15:10 +0000

    turned into funcation and added time stamp

--- task_prep.py
+++ task_prep.py
@@ -3,66 +3,85 @@ import time
 import config
 import os.path
 import xml.dom.minidom as dom
+from glob import glob
+import datetime
 
-if config.prep_xml:
 
-    for file in config.prep_xml:
+def task_prep():
+    prep_xml = glob('F:\\Transcoder\\staging\\prep\\*xml')
+    if prep_xml:
+        print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + str(prep_xml))
 
-        move_time = time.ctime()
-        doc = dom.parse(file)
-        filename = doc.getElementsByTagName("source_filename")
-        fname = filename[0].firstChild.nodeValue
-        base = os.path.splitext(os.path.basename(file))[0]
-        src_repo = config.repo + fname + '.mp4'
-        scr_tar = config.prep + base + '.mp4'
-        xml_base = os.path.basename(file)
-        vid_base = os.path.basename(scr_tar)
-        nc_1 = len(os.listdir(config.node_1_path))
-        nc_2 = len(os.listdir(config.node_2_path))
-        nc_3 = len(os.listdir(config.node_3_path))
-        nc_4 = len(os.listdir(config.node_4_path))
+        for file in prep_xml:
+            print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + str(file))
 
-        load = [nc_1, nc_2, nc_3, nc_4]
+            move_time = time.ctime()
+            doc = dom.parse(file)
+            filename = doc.getElementsByTagName("source_filename")
+            fname = filename[0].firstChild.nodeValue
+            base = os.path.splitext(os.path.basename(file))[0]
+            src_repo = config.repo + fname + '.mp4'
+            scr_tar = config.prep + base + '.mp4'
+            xml_base = os.path.basename(file)
+            vid_base = os.path.basename(scr_tar)
+            nc_1 = len(os.listdir(config.node_1_path))
+            nc_2 = len(os.listdir(config.node_2_path))
+            nc_3 = len(os.listdir(config.node_3_path))
+            nc_4 = len(os.listdir(config.node_4_path))
 
-        if min(load) == nc_1:
+            load = [nc_1, nc_2, nc_3, nc_4]
 
-            print("Task assigned to Node 1")
+            if min(load) == nc_1:
 
-            shutil.copy(src_repo, scr_tar)
-            shutil.move(file, config.node_1_path)
-            shutil.move(scr_tar, config.node_1_path)
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': '"Task assigned to Node 1")
 
-            print(move_time + ": The following files have been moved to Node 1:" '\n' + xml_base + '\n' + vid_base)
+                shutil.copy(src_repo, scr_tar)
+                shutil.move(file, config.node_1_path)
+                shutil.move(scr_tar, config.node_1_path)
 
-        elif min(load) == nc_2:
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + move_time +
+                      ": The following files have been moved to Node 1:" '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base +
+                      '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base)
 
-            print("Task assigned to Node 2")
+            elif min(load) == nc_2:
 
-            shutil.copy(src_repo, scr_tar)
-            shutil.move(file, config.node_2_path)
-            shutil.move(scr_tar, config.node_2_path)
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + "Task assigned to Node 2")
 
-            print(move_time + ": The following files have been moved to Node 2:" '\n' + xml_base + '\n' + vid_base)
+                shutil.copy(src_repo, scr_tar)
+                shutil.move(file, config.node_2_path)
+                shutil.move(scr_tar, config.node_2_path)
 
-        elif min(load) == nc_3:
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + move_time +
+                      ": The following files have been moved to Node 2:" '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base)
 
-            print("Task assigned to Node 3")
+            elif min(load) == nc_3:
 
-            shutil.copy(src_repo, scr_tar)
-            shutil.move(file, config.node_3_path)
-            shutil.move(scr_tar, config.node_3_path)
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + "Task assigned to Node 3")
 
-            print(move_time + ": The following files have been moved to Node 3:" '\n' + xml_base + '\n' + vid_base)
+                shutil.copy(src_repo, scr_tar)
+                shutil.move(file, config.node_3_path)
+                shutil.move(scr_tar, config.node_3_path)
 
-        elif min(load) == nc_4:
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + move_time +
+                      ": The following files have been moved to Node 3:" '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base)
 
-            print("Task assigned to Node 4")
+            elif min(load) == nc_4:
 
-            shutil.copy(src_repo, scr_tar)
-            shutil.move(file, config.node_4_path)
-            shutil.move(scr_tar, config.node_4_path)
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + "Task assigned to Node 4")
 
-            print(move_time + ": The following files have been moved to Node 4:" '\n' + xml_base + '\n' + vid_base)
+                shutil.copy(src_repo, scr_tar)
+                shutil.move(file, config.node_4_path)
+                shutil.move(scr_tar, config.node_4_path)
 
-else:
-    print('No files')
+                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + move_time +
+                      ": The following files have been moved to Node 4:" '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' +
+                      str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base)
+
+    else:
+        print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + 'No files')

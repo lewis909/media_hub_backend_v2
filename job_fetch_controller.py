ee8aa33783b172bd12a5063ab985979805880b61
lewis909 <lewis.dt@gmail.com>
2016-12-20 10:29:10 +0000

    updated job_fetch

--- job_fetch_controller.py
+++ job_fetch_controller.py
@@ -11,31 +11,26 @@ while len(path) >= 0:
     log = open('log_file.txt', 'a')
 
     if int(len(path)):
-        data = (str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': files')
+        data = (str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': Processing files\n')
         print(data)
         log.write(data + '\n')
         task = task_prep.task_prep()
         log.write(task)
         path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
         time.sleep(3)
-
     else:
-        #data = (str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': no files')
-        #print(data)
-        #log.write(data + '\n')
-        #log.close()
         log.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': Waiting for files\n'))
         time.sleep(5)
         path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
         if path == 0:
             log.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': No Files found, waiting for next run\n'))
             break
-    print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': no files'))
-    log.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': no files'))
-    log.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': Ending process'))
+    print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': no files\n'))
+    log.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': no files\n'))
+    log.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': Ending process\n'))
     log.close()
 
 else:
-    print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': Ending process'))
+    print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + ': Ending process\n'))
 
 

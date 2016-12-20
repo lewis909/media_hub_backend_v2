7c35fd836f1552708db5c185361c7bb6a0690347
lewis909 <lewis.dt@gmail.com>
2016-12-20 10:26:52 +0000

    tidied up project

--- controller.py
+++ controller.py
@@ -1,10 +1,6 @@
-import threading
 import transcode_engine
 import config
 
 
-
-tcn_1 = threading.Thread(target=transcode_engine.transcoder, args=(config.node_1_path, config.cursor_1, config.dbc1))
-
-tcn_1.start()
-tcn_1.join()
+while True:
+    transcode_engine.transcoder(config.node_1_path, config.cursor_1, config.dbc1)

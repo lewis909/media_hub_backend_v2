052af79659f5cea0508291734eeaf5bbc9e9a984
lewis909 <lewis.dt@gmail.com>
2017-01-26 11:57:23 +0000

    incorporated logging method

--- job_fetch_controller.py
+++ job_fetch_controller.py
@@ -1,30 +1,43 @@
-from glob import glob
 import task_prep
 import time
 import os
+import logging.handlers
 
 from functions import timestamp
+from glob import glob
 
 # Staging Path
 path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
 
+# Logging
+date_stamp = timestamp()[:11].replace('-', '_')
+file_log = logging.getLogger()
+file_log.setLevel(logging.DEBUG)
+fh = logging.handlers.RotatingFileHandler(filename='F:\\Transcoder\\logs\\move_logs\\move_log_' + date_stamp + '.txt',
+                                          maxBytes=10000000, backupCount=5)
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+fh.setFormatter(formatter)
+file_log.addHandler(fh)
+
+
 # Checks path for XML file, if an XML is found task_prep() is executed.
 while True:
 
-    with open('test_log.txt', 'a') as log:
         if int(len(path)):
             data = (timestamp() + ': Processing files\n')
             print(data)
-            log.write(data + '\n')
+            file_log.info(data)
             for i in path:
                 try:
                     task = task_prep.task_prep()
-                    log.write(task)
+                    file_log.info(task)
                     path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
                     time.sleep(3)
                 except Exception as e:
                     print(str(e))
                     print('Deleting problem file ' + str(i))
+                    file_log.error(str(e))
+                    file_log.warning('Deleting problem file ' + str(i))
                     os.remove(i)
                     time.sleep(3)
                     path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
@@ -32,4 +45,4 @@ while True:
             time.sleep(5)
             path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
             print(timestamp() + ': Waiting for files\n')
-            log.write(timestamp() + ': Waiting for files\n')
+            file_log.info(': Waiting for files\n')
--- task_prep.py
+++ task_prep.py
@@ -1,19 +1,17 @@
 import shutil
-import time
 import config
 import os.path
 import xml.dom.minidom as dom
 from glob import glob
-import datetime
 
 
 def task_prep():
     prep_xml = glob('F:\\Transcoder\\staging\\prep\\*xml')
     if prep_xml:
-        print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + str(prep_xml))
+        print(': ' + str(prep_xml))
 
         for file in prep_xml:
-            print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + str(file))
+            print(': ' + str(file))
 
             doc = dom.parse(file)
             filename = doc.getElementsByTagName("source_filename")
@@ -32,43 +30,35 @@ def task_prep():
 
             if min(load) == nc_1:
 
-                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': '"Task assigned to Node 1")
-
                 shutil.copy(src_repo, scr_tar)
                 shutil.move(file, config.node_1_path)
                 shutil.move(scr_tar, config.node_1_path)
 
-                return str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' +  ": The following files have been moved to Node 1:" '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base + '\n'
+                return ': The following files have been moved to Node 1:' + xml_base + ', ' + vid_base + '\n'
 
             elif min(load) == nc_2:
-
-                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + "Task assigned to Node 2")
 
                 shutil.copy(src_repo, scr_tar)
                 shutil.move(file, config.node_2_path)
                 shutil.move(scr_tar, config.node_2_path)
 
-                return str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + ": The following files have been moved to Node 2:" '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base + '\n'
+                return ': The following files have been moved to Node 2:' + xml_base + ', ' + vid_base + '\n'
 
             elif min(load) == nc_3:
-
-                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + "Task assigned to Node 3")
 
                 shutil.copy(src_repo, scr_tar)
                 shutil.move(file, config.node_3_path)
                 shutil.move(scr_tar, config.node_3_path)
 
-                return str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' +  ": The following files have been moved to Node 3:" '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base + '\n'
+                return ': The following files have been moved to Node 3:' + xml_base + ', ' + vid_base + '\n'
 
             elif min(load) == nc_4:
-
-                print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + "Task assigned to Node 4")
 
                 shutil.copy(src_repo, scr_tar)
                 shutil.move(file, config.node_4_path)
                 shutil.move(scr_tar, config.node_4_path)
 
-                return str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' +  ": The following files have been moved to Node 4:" '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + xml_base + '\n' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + vid_base + '\n'
+                return ': The following files have been moved to Node 4:' + xml_base + ', ' + vid_base + '\n'
 
     else:
-        print(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + ': ' + 'No files')
+        print(': ' + 'No files')

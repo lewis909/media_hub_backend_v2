714c0d23d985045e2783e7e2031a32679d046909
lewis909 <lewis.dt@gmail.com>
2017-01-27 16:02:39 +0000

    added sleep function

--- job_fetch_controller.py
+++ job_fetch_controller.py
@@ -23,27 +23,35 @@ file_log.addHandler(fh)
 # Checks path for XML file, if an XML is found task_prep() is executed.
 while True:
 
-        if int(len(path)):
-            data = ': Processing files\n'
-            print(timestamp() + data)
-            file_log.info(data)
-            for i in path:
-                try:
-                    task = task_prep.task_prep()
-                    print(timestamp() + task)
-                    file_log.info(task)
-                    path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
-                    time.sleep(3)
-                except Exception as e:
-                    print(str(e))
-                    print('Deleting problem file ' + str(i))
-                    file_log.error(str(e))
-                    file_log.warning('Deleting problem file ' + str(i))
-                    os.remove(i)
-                    time.sleep(3)
-                    path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
+    for s in range(30):
+
+        if s == 15:
+            print(timestamp() + ': sleeping\n')
+            file_log.info(': sleeping\n')
+            time.sleep(30)
         else:
-            time.sleep(5)
-            path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
-            print(timestamp() + ': Waiting for files\n')
-            file_log.info(': Waiting for files\n')
+
+            if int(len(path)) > 0:
+                data = ': Processing files\n'
+                print(timestamp() + data)
+                file_log.info(data)
+                for i in path:
+                    try:
+                        task = task_prep.task_prep()
+                        print(timestamp() + task)
+                        file_log.info(task)
+                        path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
+                        time.sleep(3)
+                    except Exception as e:
+                        print(str(e))
+                        print('Deleting problem file ' + str(i))
+                        file_log.error(str(e))
+                        file_log.warning('Deleting problem file ' + str(i))
+                        os.remove(i)
+                        time.sleep(3)
+                        path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
+            else:
+                time.sleep(1)
+                path = glob('F:\\Transcoder\\staging\\prep\\*.xml')
+                print(timestamp() + ': Waiting for files\n')
+                file_log.info(': Waiting for files\n')

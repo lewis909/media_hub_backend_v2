1480bf2745a3c729d4317d110dbe69d332536322
lewis909 <lewis.dt@gmail.com>
2017-01-30 08:17:15 +0000

    added new DB

--- config.py
+++ config.py
@@ -19,35 +19,59 @@ mp4_src = glob('F:\\Transcoder\\staging\\prep\\*mp4')
 prog_temp = os.path.join('F:\\Transcoder\\logs\\transcode_logs\\temp\\')
 transcode_logs = os.path.join('F:\\Transcoder\\logs\\transcode_logs\\')
 
-
 # Database Connections
-host = 'localhost'
-user = 'lewis_transcode'
-password = 'tool4602'
-database = 'media_hub'
+# Media hub Connection
+mh_host = 'localhost'
+mh_user = 'lewis_transcode'
+mh_password = 'tool4602'
+mh_database = 'media_hub'
+
+# Txmam Connection
+tx_host = 'localhost'
+tx_user = 'lewis_transcode'
+tx_password = 'tool4602'
+tx_database = 'txmam'
+
+# Schedule Connection
+sc_host = 'localhost'
+sc_user = 'lewis_transcode'
+sc_password = 'tool4602'
+sc_database = 'schedule'
 
 # Connection 1
-dbc1 = mariadb.connect(host=host,
-                       user=user,
-                       password=password,
-                       database=database)
+dbc1 = mariadb.connect(host=mh_host,
+                       user=mh_user,
+                       password=mh_password,
+                       database=mh_database)
 # Connection 2
-dbc2 = mariadb.connect(host=host,
-                       user=user,
-                       password=password,
-                       database=database)
+dbc2 = mariadb.connect(host=mh_host,
+                       user=mh_user,
+                       password=mh_password,
+                       database=mh_database)
 # Connection 3
-dbc3 = mariadb.connect(host=host,
-                       user=user,
-                       password=password,
-                       database=database)
+dbc3 = mariadb.connect(host=mh_host,
+                       user=mh_user,
+                       password=mh_password,
+                       database=mh_database)
 # Connection 4
-dbc4 = mariadb.connect(host=host,
-                       user=user,
-                       password=password,
-                       database=database)
+dbc4 = mariadb.connect(host=mh_host,
+                       user=mh_user,
+                       password=mh_password,
+                       database=mh_database)
+# txmam Connection
+dbctx = mariadb.connect(host=tx_host,
+                        user=tx_user,
+                        password=tx_password,
+                        database=tx_database)
+# schedule Connection
+dbcsc = mariadb.connect(host=sc_host,
+                        user=sc_user,
+                        password=sc_password,
+                        database=sc_database)
 # DB Cursors
 cursor_1 = dbc1.cursor()
 cursor_2 = dbc2.cursor()
 cursor_3 = dbc3.cursor()
 cursor_4 = dbc4.cursor()
+cursor_tx = dbctx.cursor()
+cursor_sc = dbcsc.cursor()
